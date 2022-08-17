2234789dc94b70db2b97dc2f06f3cf6daf6db5b8
Harry Law <53987565+h5law@users.noreply.github.com>
2022-08-17 13:29:32 +0100

    Add start subcommand to handle starting server

--- cmd/root.go
+++ cmd/root.go
@@ -29,17 +29,14 @@ import (
 )
 
 var (
-	port    int
 	verbose bool
-	logFile string
 
 	rootCmd = &cobra.Command{
 		Use:   "paste-server",
-		Short: "Start a paste-server instance locally",
-		Long: `Start a paste-server instance to interact with a MongoDB
-database to create, read, update and delete temporary pastes`,
-		Version: "ALPHA-0.1.1",
-		//Run: func(cmd *cobra.Command, args []string) {}
+		Short: "paste-server is a temporary pastebin for sharing files",
+		Long: `paste-server instances allow users to interact with a MongoDB
+database to create, read, update and delete temporary pastes, which can be
+shared and viewed by anyone through their UUIDs.`,
 	}
 )
 
@@ -50,29 +47,13 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().IntVarP(
-		&port,
-		"port",
-		"p",
-		3000, "port to run the server on",
-	)
 	rootCmd.PersistentFlags().BoolVarP(
 		&verbose,
 		"verbose",
 		"v",
 		false, "print verbose logs",
 	)
-	rootCmd.PersistentFlags().StringVarP(
-		&logFile,
-		"logFile",
-		"l",
-		"", "path to log file",
-	)
 
-	viper.BindPFlag("port", rootCmd.PersistentFlags().Lookup("port"))
 	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
-	viper.BindPFlag("logFile", rootCmd.PersistentFlags().Lookup("logFile"))
-	viper.SetDefault("port", 3000)
 	viper.SetDefault("verbose", "false")
-	viper.SetDefault("logFile", "")
 }
---
+++ cmd/start.go
@@ -0,0 +1,162 @@
+/*
+Copyright © 2022 Harry Law <hrryslw@pm.me>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/h5law/paste-server/api"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	port    int
+	logFile string
+
+	startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start a paste-server http server",
+		Long: `The start subcommand will start a paste-server instance on
+the given port or default to using 3000.
+
+If no logfile is given logs will be directed to os.Stdout - if a logfile is
+provided logs will be appended to that file (creating it if it doesn't exist).`,
+		Run: func(cmd *cobra.Command, args []string) {
+			prepareServer()
+		},
+	}
+)
+
+// Load environment varaibles
+func goDotEnvVariable(key string) string {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+func prepareServer() {
+	// Enable graceful shutdown on signal interrupts
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Listen for interrupt
+	go func() {
+		oscall := <-c
+		log.Printf("system call: %v\n", oscall)
+		cancel()
+	}()
+
+	if err := startServer(ctx); err != nil {
+		log.Fatalf("failed to start server: %v\n", err)
+	}
+}
+
+func startServer(ctx context.Context) error {
+	port := viper.GetInt("port")
+	portStr := fmt.Sprintf(":%d", port)
+
+	// Load connection URI for mongo from .env
+	uri := goDotEnvVariable("MONGO_URI")
+	if uri == "" {
+		log.Fatal("Unable to extract 'MONGO_URI' environment variable")
+	}
+
+	h := api.NewHandler()
+
+	log.Println("starting server")
+
+	srv := &http.Server{
+		Addr:         portStr,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      h,
+	}
+
+	// Start server in go routine so non-blocking
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen error: %v\n", err)
+		}
+	}()
+
+	log.Printf("paste-server started on %v\n", portStr)
+
+	// Connect to MongoDB and defer disconnection
+	h.ConnectDB(uri)
+
+	// Context has been cancelled - stop everything
+	<-ctx.Done()
+
+	log.Println("stopping server")
+
+	// Create context and shutdown server
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	h.DisconnectDB()
+	err := srv.Shutdown(ctxShutdown)
+	if err != nil {
+		log.Fatalf("server shutdown failed: %v\n", err)
+	}
+
+	log.Println("paste-server stopped")
+
+	if err == http.ErrServerClosed {
+		return nil
+	}
+
+	return err
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	startCmd.Flags().IntVarP(
+		&port,
+		"port",
+		"p",
+		3000, "port to run the server on",
+	)
+	startCmd.Flags().StringVarP(
+		&logFile,
+		"logfile",
+		"l",
+		"", "path to log file",
+	)
+
+	viper.BindPFlag("port", startCmd.Flags().Lookup("port"))
+	viper.BindPFlag("logfile", startCmd.Flags().Lookup("logFile"))
+	viper.SetDefault("port", 3000)
+	viper.SetDefault("logFile", "")
+}
