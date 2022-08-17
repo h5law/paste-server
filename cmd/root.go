3da010435aa2c6290ffb70dcbb0cd3da14e1ca51
Harry Law <53987565+h5law@users.noreply.github.com>
2022-08-17 11:18:05 +0100

    Add license comment block to code

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,30 @@
+/*
+Copyright © 2022 Harry Law <hrryslw@pm.me>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -15,14 +39,19 @@ var (
 		Short: "Start a paste-server instance locally",
 		Long: `Start a paste-server instance to interact with a MongoDB
 database to create, read, update and delete temporary pastes`,
+		Version: "ALPHA-0.1.1",
 		Run: func(cmd *cobra.Command, args []string) {
 			return
-		},
+		}
 	}
 )
 
-func Execute() error {
-	return rootCmd.Execute()
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(err)
 }
 
 func init() {
